8760dc7c6a3225b380ae0fd107b778c2b1c15878
ANDREYGSM <andrey_santa@soy.sena.edu.co>
2024-08-20 13:32:33 -0500

    no me acorde de mucho jaja

---
+++ algoritmocompra.py
@@ -0,0 +1,9 @@
+
+precio_jean = 125.000
+cantidad_jean = 2
+precio_camisa = 45.000
+cantiadad_camisa = 2
+precio_camisa_tipopolo = 30.000
+cantiadad_camisa_tipopolo = 1
+descuento_compra = 3
+total_compra = (precio_camisa+precio_camisa_tipopolo+precio_jean)
